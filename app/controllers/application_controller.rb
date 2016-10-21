5507b44d6aca14cb83966e4cb401804ee8b93005
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-21 09:33:10 +0000

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,7 +112,7 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
 
   def resp(status, json={})
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,21 @@
 class PagesController < ActionController::Base
-  def index
-  end
-
   def signin
     @user = User.new
+    render :signin, layout: layout_for_request
   end
 
   def signup
     @user = User.new
+    render :signup, layout: layout_for_request
+  end
+
+  private
+
+  def layout_for_request
+    if request.headers['X-PJAX'].present?
+      nil
+    else
+      'pages'
+    end
   end
 end
