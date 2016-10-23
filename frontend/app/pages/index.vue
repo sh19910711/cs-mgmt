9256dc5b86a137303bf9c72f137d674a00abab2d
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-23 05:20:42 +0000

    wip

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -8,29 +8,32 @@
       .leading-body
         p Getting started with codestand.io.
 
-    // descriptions
     .container
       .block
-        h2.block-header 1. Install CLI toolchain
+        .block-header
+          h2 1. Install CLI toolchain
         .block-body
           h3 Mac OSX
           code
             | $ brew install codestand/codestand/codestand
 
       .block
-        h2.block-header 2. Create and register your app
+        .block-header
+          h2 2. Create and register your app
         .block-body
           code
             | $ codestand new --register led-blink
 
       .block
-        h2.block-header 3. Link your device to the codestand.io
+        .block-header
+          h2 3. Link your device to the codestand.io
         .block-body
           code
             | $ codestand install
 
       .block
-        h2.block-header 4. Deploy the app to the device
+        .block-header
+          h2 4. Deploy the app to the device
         .block-body
           code
             | $ codestand deploy
